c7657b57ffac8e31d63b4d80b118ba677b1f8842
tcywinski <tcywinski@inf.ug.edu.pl>
2012-02-29 10:19:59 +0100

    info

--- mail.sh
+++ mail.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Tymoteusz Cywinski
-echo "**********************"
-echo "* wyszukiwanie plikow*"
-echo "**********************"
+echo "****************************************************"
+echo "* wyszukiwanie plikow dla studentow informatyki UG *"
+echo "****************************************************"
 echo "witaj" $USER "!"
 echo "------------------------"
 echo "wybierz gdzie chcesz szukac:"
-	    select z in "w katalogu domowym" "w podanym katalogu" "x"
+	    select z in "w katalogu domowym" "w podanym katalogu"
 	    do
 		case $z in
 		    "w katalogu domowym")
 		    echo "zostanie przeszukany katalog domowy"
-		    SCIEZKA="$HOME"
+		    SCIEZKA=$HOME
 		;;
 		    "w podanym katalogu")
 		    echo "podaj sciezke do katalogu:"
@@ -20,33 +20,38 @@ echo "wybierz gdzie chcesz szukac:"
 		;;
 		    *) echo "nic nie wybrales.."
 		       echo "zostanie przeszukany katalog domowy"
-		    SCIEZKA="$HOME"
+		    SCIEZKA=$HOME
 	        esac
 	    echo "podaj nazwe pliku lub jej czesc:"
 	    read NAZWA
 	    echo "--------------------------"
 	    echo "znaleziono pliki:"
 	    find $SCIEZKA -iname "$NAZWA" 
-	    echo "wyslac je w zalaczniku email? (t/n)"
+	    echo "przeniesc je do public_html wraz z informacja na email? (t/n)"
 	    read CZYTAK
 	    if [ "$CZYTAK" == "t" ] ; then
-		TEMAT="wyslanie pliku"
+		TEMAT="adres_do_pliku"
 		echo "podaj adres email:"
 		read EMAIL
-		EMAILMESSAGE=" "
 		echo "dodac wiadomosc tekstowa? (t/n)"
 		read CZYTAK2
+		touch message.txt
+		echo "sciezka do pliku $NAZWA.tar to:" >> message.txt
+		echo "http://sigma.inf.ug.edu.pl/~$USER/$NAZWA.tar" >> message.txt
 		if [ "$CZYTAK2" == "t" ] ; then
+		    echo "-----------------" >> message.txt
 		    echo "wprowadz tekst:"
 		    read EMAILMESSAGE
+		    echo "$EMAILMESSAGE" >> message.txt
 		fi
-		echo "kompresuje zalaczniki.."
-		touch $NAZWA.tar
+		echo "pakuje pliki.."
 		tar -rvf $NAZWA.tar `find $SCIEZKA -iname "$NAZWA"`
-		echo "wysylam emaila.."
-		/bin/mail -s "$TEMAT" -a $NAZWA.tar "$EMAIL"
+		echo "przenosze pliki do public_html.."
+		mv $NAZWA.tar ~/public_html/$NAZWA.tar
+		echo "wysylam emaila z informacja.."
+		cat message.txt | mail -s "$TEMAT" $EMAIL 
 		echo "mail wyslany."
-#		rm $NAZWA.tar
+		rm message.txt
 	    fi
 break
 done
